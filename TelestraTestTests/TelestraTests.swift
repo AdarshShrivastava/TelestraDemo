da817f0c48e3f6593bd3e7b37cfaaec92d425904
Adarsh Shrivastava <adarshshrivastava@Adarshs-MacBook-Air.local>
2018-09-19 10:17:22 +0530

    test case for testing url status code

--- TelestraTestTests/TelestraTests.swift
+++ TelestraTestTests/TelestraTests.swift
@@ -7,21 +7,23 @@
 //
 
 import XCTest
+//import TelestraViewModel
+
 @testable import TelestraTest
 class TelestraTests: XCTestCase{
+    var sessionUnderTest:URLSession!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sessionUnderTest = nil
         super.tearDown()
     }
     
     var vc = ViewController()
-    
     //Test to find whether controller has a table view
     func testControllerHasTableView() {
         vc.loadViewIfNeeded()
@@ -34,9 +36,24 @@ class TelestraTests: XCTestCase{
         XCTAssertTrue(vc.conforms(to: UITableViewDelegate.self))
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    //Test to verify URL
+    func testValidCallToGetStatusCode200(){
+        let url = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")
+        let promise = expectation(description: "status code: 200")
+        let dataTask =  sessionUnderTest.dataTask(with: url!) { (data, response, error) in
+            if let error = error{
+                XCTFail("error:\(error.localizedDescription)")
+            }else if let statusCode = (response as? HTTPURLResponse)?.statusCode{
+                if statusCode == 200{
+                    promise.fulfill()
+                }else{
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+        }
+        dataTask.resume()
+        
+        waitForExpectations(timeout: 10, handler: nil)
     }
     
     func testPerformanceExample() {
